8fd54d3b60c966b71b5beff1bcd7491b9f7d90b7
Gustav Malmqvist <gustav@zerebra.com>
2024-06-06 22:51:10 +0200

    feat: add support for malaysia itn

--- stdnum/my/__init__.py
+++ stdnum/my/__init__.py
@@ -19,3 +19,8 @@
 # 02110-1301 USA
 
 """Collection of Malaysian numbers."""
+
+# provide aliases
+from stdnum.sg import itn as vat  # noqa: F401
+from stdnum.sg import itn as business_tin  # noqa: F401
+from stdnum.sg import nric as personal_tin  # noqa: F401
